8a5d082d1c85e85ab1bcf2351da3ac58247d6b00
gourabofficial <gangulygourab3@gmail.com>
2024-09-08 19:59:34 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "e8e7b40ca343445986692837240809"; 
+const apiKey = process.env._MY_API_KEY; 
 const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=`;
 const baseUrl = 'http://api.weatherapi.com/v1/current.json?'
 
@@ -9,6 +9,8 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 
 async function getWeather(city) {
+
+
     const response = await fetch(apiUrl + city);
     const data = await response.json();
     console.log(data);
@@ -20,30 +22,13 @@ async function getWeather(city) {
     document.querySelector('.temp').innerHTML = data.current.temp_c + "°C";
     document.querySelector('.humidity').innerHTML = data.current.humidity + "%";
     document.querySelector('.wind').innerHTML = data.current.wind_kph + "km/h";
+    weatherIcon. src = data.current.condition.icon;
 
 
+   
+    }
 
-//     if (data.current.condition.text === "Sunny") {
-//         weatherIcon.src = "./All img/clear.png";
-//     } else if (data.current.condition.text === "Partly cloudy") {
-//         weatherIcon.src = "./All img/mist.png";
-//     } else if (data.current.condition.text === "Cloudy") {
-//         weatherIcon.src = "./All img/clouds.png";
-//     } else if (data.current.condition.text === "Overcast") {
-//         weatherIcon.src = "./All img/drixxle.png";
-//     } else if (data.current.condition.text === "Mist") {
-//         weatherIcon.src = "./All img/mist.png";
-//     } else if (data.current.condition.text === "Patchy rain possible") {
-//         weatherIcon.src = "./All img/rain.png";
-//     } else if (data.current.condition.text === "Patchy light rain") {
-//         weatherIcon.src = "./All img/rain.png";
-//     } else if (data.current.condition.text === "Light rain") {
-//         weatherIcon.src = "./All img/rain.png";
-//     } else if (data.current.condition.text === "Moderate or heavy rain shower") {
-//         weatherIcon.src = "./All img/rain.png";
-//     }
-
- }
+ 
 
 
 searchBtn.addEventListener("click", () => {

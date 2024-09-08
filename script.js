eda43e11923b9109276d9409f25e0ba0ae0bdae8
gourabofficial <gangulygourab3@gmail.com>
2024-09-08 17:10:11 +0530

    updated

--- script.js
+++ script.js
@@ -4,17 +4,47 @@ const baseUrl = 'http://api.weatherapi.com/v1/current.json?'
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
 
 async function getWeather(city) {
     const response = await fetch(apiUrl + city);
     const data = await response.json();
     console.log(data);
 
+
+
+
     document.querySelector('.city').innerHTML = data.location.name;
     document.querySelector('.temp').innerHTML = data.current.temp_c + "°C";
     document.querySelector('.humidity').innerHTML = data.current.humidity + "%";
     document.querySelector('.wind').innerHTML = data.current.wind_kph + "km/h";
-}
+
+
+
+//     if (data.current.condition.text === "Sunny") {
+//         weatherIcon.src = "./All img/clear.png";
+//     } else if (data.current.condition.text === "Partly cloudy") {
+//         weatherIcon.src = "./All img/mist.png";
+//     } else if (data.current.condition.text === "Cloudy") {
+//         weatherIcon.src = "./All img/clouds.png";
+//     } else if (data.current.condition.text === "Overcast") {
+//         weatherIcon.src = "./All img/drixxle.png";
+//     } else if (data.current.condition.text === "Mist") {
+//         weatherIcon.src = "./All img/mist.png";
+//     } else if (data.current.condition.text === "Patchy rain possible") {
+//         weatherIcon.src = "./All img/rain.png";
+//     } else if (data.current.condition.text === "Patchy light rain") {
+//         weatherIcon.src = "./All img/rain.png";
+//     } else if (data.current.condition.text === "Light rain") {
+//         weatherIcon.src = "./All img/rain.png";
+//     } else if (data.current.condition.text === "Moderate or heavy rain shower") {
+//         weatherIcon.src = "./All img/rain.png";
+//     }
+
+ }
+
 
 searchBtn.addEventListener("click", () => {
     if(searchBox.value === "") {
